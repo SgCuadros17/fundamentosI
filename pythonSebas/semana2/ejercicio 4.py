7f615050a0c1bbe95000205cb916901a611d35da
B09S301est <sebastian.garces@upb.edu.co>
2026-02-11 21:06:22 -0500

    add ejercicios 4

---
+++ pythonSebas/semana2/ejercicio 4.py
@@ -0,0 +1,28 @@
+#variables type lista "[]" y type diicionario
+personas={"nombre": "edad", "edad": 30, "ciudad": "medellin"}
+print(f"tu nombre es {personas['nombre']}, y tienes {personas['edad']} y vives en {personas['ciudad']}")
+
+#reglas para definir un variable en Python
+#Deben empezar con letra o gion bajo(no puede iniciar con números).
+#
+
+
+#Concatenación de Cadenas (Strings)
+#-------------------------------------------
+#Las variables de tipo `string` pueden combinarse con el operador `+`:
+# nombre = "Sofía"
+# apellido = "Martínez"
+# nombre_completo = nombre + " " + apellido  # Concatenación
+# print(nombre_completo)  # Salida: Sofía Martínez
+#-----------------------------------------
+#También se pueden usar f-strings para formatear textos:
+# nombre= "sebastian"
+# edad = 22
+# mensaje = f"Hola, me llamo {nombre} y tengo {edad} años."
+# print(mensaje)
+#------------------------------------------
+#variables type contador:
+# contador = 0
+# contador += 1  # Es lo mismo que: contador = contador + 1
+# print("Contador actualizado:", contador)
+
